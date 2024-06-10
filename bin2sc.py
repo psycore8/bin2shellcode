4fc9d9be762007f4fd69e6921a50088e97ec16e0
psycore8 <76014584+psycore8@users.noreply.github.com>
2024-06-10 17:08:18 +0200

    Update bin2sc.py
    added python3 compatibility

--- bin2sc.py
+++ bin2sc.py
@@ -1,36 +1,39 @@
 #!/usr/bin/env python
 import sys
 if __name__ == "__main__":
-	if len(sys.argv) < 3:
-		print "usage: %s file.bin c|cs\n" % (sys.argv[0],)
-		sys.exit(0)
+ if len(sys.argv) < 3:
+  print("usage: %s file.bin c|cs\n" % (sys.argv[0],))
+  sys.exit(0)
 
-	if sys.argv[2] == "c":
-		# for c shellcode
-		shellcode = "\""
-		ctr = 1
-		maxlen = 15
+ if sys.argv[2] == "c":
+  # for c shellcode
+  shellcode = "\""
+  ctr = 1
+  maxlen = 15
 
-		for b in open(sys.argv[1], "rb").read():
-			shellcode += "\\x" + b.encode("hex")
-			if ctr == maxlen:
-				shellcode += "\" \n\""
-				ctr = 0
-			ctr += 1
-		shellcode += "\""
-		print shellcode
+  for b in open(sys.argv[1], "rb").read():
+   #shellcode += "\\x" + b.encode("hex")
+   hex_val = hex(b)
+   shellcode += "\\x" + hex_val[2:]
+   if ctr == maxlen:
+    shellcode += "\" \n\""
+    ctr = 0
+   ctr += 1
+  shellcode += "\""
+  print(shellcode)
 
-	else:
-		# for cs shellcode
-		shellcode = ""
-		ctr = 1
-		maxlen = 15
-		
-		for b in open(sys.argv[1], "rb").read():
-			shellcode += "0x" + b.encode("hex") + ","
-			if ctr == maxlen:
-				shellcode += "\n"
-				ctr = 0
-			ctr += 1
+ else:
+  # for cs shellcode
+  shellcode = ""
+  ctr = 1
+  maxlen = 15
+  
+  for b in open(sys.argv[1], "rb").read():
+   #shellcode += "0x" + b.encode("hex") + ","
+   shellcode += hex(b) + ","
+   if ctr == maxlen:
+    shellcode += "\n"
+    ctr = 0
+   ctr += 1
 
-		print shellcode
+  print(shellcode)
